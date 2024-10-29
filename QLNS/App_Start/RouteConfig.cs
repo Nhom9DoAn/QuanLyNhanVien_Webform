33734dddfb0244537207c7d4dc298c420eae6937
Huỳnh <dangnhuh2003@gmail.com>
2024-10-29 23:07:11 +0700

    build theme admin + candidate

--- QLNS/App_Start/RouteConfig.cs
+++ QLNS/App_Start/RouteConfig.cs
@@ -11,17 +11,12 @@ namespace QLNS
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var settings = new FriendlyUrlSettings();
-            settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
-
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
-                    name: "Default",
-                    url: "{controller}/{action}/{id}",
-                    defaults: new { action = "Index", id = UrlParameter.Optional }
-                );  
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
